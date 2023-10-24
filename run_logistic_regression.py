602c67b9d42110a37c38bc6a8f42d82689103be1
dlangto <dlangto@clemson.edu>
2023-10-24 12:08:59 -0400

    10/24

--- run_logistic_regression.py
+++ run_logistic_regression.py
@@ -5,12 +5,12 @@ dataset = pandas.read_csv("dataset.csv")
 
 print(dataset)
 
-target = dataset.iloc[:,0].values
+target = dataset.iloc[:,1].values
 # sets y values from first three columns
 data = dataset.iloc[:,3:9].values
 # sets x values from columns 3-8
 
-machine = linear_model.LinearRegression()
+machine = linear_model.LogisticRegression()
 machine.fit(data,target)
 #make a linear regression for x, y
 
